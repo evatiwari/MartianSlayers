660b7d168cd06ebff3662c032eb431ca92e63d04
Eva <eva.tiwari@gmail.com>
2020-07-09 21:29:05 +0530

    Fill in the functions ending with 4 accordingly

--- public/javascripts/three.js
+++ public/javascripts/three.js
@@ -3,8 +3,9 @@ const player = "O";
 const computer = "X";
 
 let board_full = false;
-//the board, in an array form
+//the boards, in an array form
 let threeboard = ["", "", "", "", "", "", "", "", ""];
+let fourboard = ["", "", "", "","", "", "", "","", "", "", "","", "", "", ""];
 //contains the board class
 const board_container = document.querySelector(".board");
 //contains the winner section
@@ -20,19 +21,27 @@ check_board_complete = () => {
   board_full = flag;
 };
 
-//checks if any three elements are the same
-const check_line = (a, b, c) => {
+//checks if any n elements are the same
+const check_line_3 = (a, b, c) => {
   return (
     threeboard[a] == threeboard[b] &&
     threeboard[b] == threeboard[c] &&
     (threeboard[a] == player || threeboard[a] == computer)
   );
 };
-//checks for winner and returns the winner
-const check_match = () => {
+const check_line_4 = (a, b, c, d) => {
+  return (
+    fourboard[a] == fourboard[b] &&
+    fourboard[b] == fourboard[c] &&
+    fourboard[c] == fourboard[d] &&
+    (threeboard[a] == player || threeboard[a] == computer)
+  );
+};
+//checks for winner of 3x3 and returns the winner
+const check_match_3 = () => {
     //checking for complete rows
   for (i = 0; i < 9; i += 3) {
-    if (check_line(i, i + 1, i + 2)) {
+    if (check_line_3(i, i + 1, i + 2)) {
       document.querySelector(`#b${i}`).classList.add("win");
       document.querySelector(`#b${i + 1}`).classList.add("win");
       document.querySelector(`#b${i + 2}`).classList.add("win");
@@ -41,7 +50,7 @@ const check_match = () => {
   }
   //checking for complete columns
   for (i = 0; i < 3; i++) {
-    if (check_line(i, i + 3, i + 6)) {
+    if (check_line_3(i, i + 3, i + 6)) {
       document.querySelector(`#b${i}`).classList.add("win");
       document.querySelector(`#b${i + 3}`).classList.add("win");
       document.querySelector(`#b${i + 6}`).classList.add("win");
@@ -49,14 +58,14 @@ const check_match = () => {
     }
   }
   //checking left diagonal
-  if (check_line(0,4,8)) {
+  if (check_line_3(0,4,8)) {
     document.querySelector("#b0").classList.add("win");
     document.querySelector("#b4").classList.add("win");
     document.querySelector("#b8").classList.add("win");
     return threeboard[0];
   }
   //checking right diagonal
-  if (check_line(2, 4, 6)) {
+  if (check_line_3(2, 4, 6)) {
     document.querySelector("#b2").classList.add("win");
     document.querySelector("#b4").classList.add("win");
     document.querySelector("#b6").classList.add("win");
@@ -67,7 +76,7 @@ const check_match = () => {
 };
 
 const check_for_winner = () => {
-  let res = check_match()
+  let res = check_match_3()
   if (res == player) {
     winner.innerText = "You win!";
     winner.classList.add("playerWin");
@@ -83,49 +92,88 @@ const check_for_winner = () => {
 };
 
 
-const render_board = () => {
+const render_board_3 = () => {
   board_container.innerHTML = ""
   threeboard.forEach((e, i) => {
-    board_container.innerHTML += `<div id="b${i}" class="block" onclick="addPlayerMove(${i})">${threeboard[i]}</div>`
+    board_container.innerHTML += `<div id="c${i}" class="block" onclick="addPlayerMove3(${i})">${threeboard[i]}</div>`
     if (e == player || e == computer) {
       document.querySelector(`#b${i}`).classList.add("occupied");
     }
   });
 };
+const render_board_4 = () => {
+  board_container.innerHTML = ""
+  fourboard.forEach((e, i) => {
+    board_container.innerHTML += `<div id="b${i}" class="block" onclick="addPlayerMove4(${i})">${fourboard[i]}</div>`
+    if (e == player || e == computer) {
+      document.querySelector(`#c${i}`).classList.add("occupied");
+    }
+  });
+};
 
-const game_loop = () => {
-  render_board();
+const game_loop_3 = () => {
+  render_board_3();
   check_board_complete();
   check_for_winner();
 }
-
-const addPlayerMove = e => {
+const game_loop_4 = () => {
+  render_board_4();
+  check_board_complete();
+  check_for_winner();
+}
+//adding player moves
+const addPlayerMove3 = e => {
   if (!board_full && threeboard[e] == "") {
     threeboard[e] = player;
-    game_loop();
-    addComputerMove();
+    game_loop_3();
+    addComputerMove3();
+  }
+};
+const addPlayerMove4 = e => {
+  if (!board_full && fourboard[e] == "") {
+    fourboard[e] = player;
+    game_loop_4();
+    addComputerMove4();
   }
 };
 //Aishwwarya, make changes here, this is entirely random as of now
-const addComputerMove = () => {
+const addComputerMove3 = () => {
   if (!board_full) {
     do {
       selected = Math.floor(Math.random() * 9);
     } while (threeboard[selected] != "");
     threeboard[selected] = computer;
-    game_loop();
+    game_loop_3();
   }
 };
-
-const reset_board = () => {
+const addComputerMove4 = () => {
+  if (!board_full) {
+    do {
+      selected = Math.floor(Math.random() * 16);
+    } while (fourboard[selected] != "");
+    fourboard[selected] = computer;
+    game_loop_4();
+  }
+};
+//reset board
+const reset_board_3 = () => {
   threeboard = ["", "", "", "", "", "", "", "", ""];
   board_full = false;
   winner.classList.remove("playerWin");
   winner.classList.remove("computerWin");
   winner.classList.remove("draw");
   winner.innerText = "";
-  render_board();
+  render_board_3();
+};
+const reset_board_4 = () => {
+  fourboard = ["", "", "", "","", "", "", "","", "", "", "","", "", "", ""];
+  board_full = false;
+  winner.classList.remove("playerWin");
+  winner.classList.remove("computerWin");
+  winner.classList.remove("draw");
+  winner.innerText = "";
+  render_board_4();
 };
 
 //initial render
-render_board();
+render_board_3();
