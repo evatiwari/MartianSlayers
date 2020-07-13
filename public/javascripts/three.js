34b48089b35ab5f4ce373743fe3855525d9a4182
AishwwaryaM <58990857+AishwwaryaM@users.noreply.github.com>
2020-07-13 09:51:36 +0530

    added vsHuman, level

--- public/javascripts/three.js
+++ public/javascripts/three.js
@@ -1,7 +1,13 @@
 //Aishwwarya, please make changes here
-const player = "O";
-const computer = "X";
-
+var player = "X";
+var computer = "O";
+var level = 1;
+let tot = 0;
+var vsHuman  = 0;
+var player1 = "X";
+var player2 = "O";
+var move =1;
+var selected;
 let board_full = false;
 //the boards, in an array form
 let threeboard = ["", "", "", "", "", "", "", "", ""];
@@ -42,33 +48,33 @@ const check_match_3 = () => {
     //checking for complete rows
   for (i = 0; i < 9; i += 3) {
     if (check_line_3(i, i + 1, i + 2)) {
-      document.querySelector(`#b${i}`).classList.add("win");
-      document.querySelector(`#b${i + 1}`).classList.add("win");
-      document.querySelector(`#b${i + 2}`).classList.add("win");
+      document.querySelector(`#c${i}`).classList.add("win");
+      document.querySelector(`#c${i + 1}`).classList.add("win");
+      document.querySelector(`#c${i + 2}`).classList.add("win");
       return threeboard[i];
     }
   }
   //checking for complete columns
   for (i = 0; i < 3; i++) {
     if (check_line_3(i, i + 3, i + 6)) {
-      document.querySelector(`#b${i}`).classList.add("win");
-      document.querySelector(`#b${i + 3}`).classList.add("win");
-      document.querySelector(`#b${i + 6}`).classList.add("win");
+      document.querySelector(`#c${i}`).classList.add("win");
+      document.querySelector(`#c${i + 3}`).classList.add("win");
+      document.querySelector(`#c${i + 6}`).classList.add("win");
       return threeboard[i];
     }
   }
   //checking left diagonal
   if (check_line_3(0,4,8)) {
-    document.querySelector("#b0").classList.add("win");
-    document.querySelector("#b4").classList.add("win");
-    document.querySelector("#b8").classList.add("win");
+    document.querySelector("#c0").classList.add("win");
+    document.querySelector("#c4").classList.add("win");
+    document.querySelector("#c8").classList.add("win");
     return threeboard[0];
   }
   //checking right diagonal
   if (check_line_3(2, 4, 6)) {
-    document.querySelector("#b2").classList.add("win");
-    document.querySelector("#b4").classList.add("win");
-    document.querySelector("#b6").classList.add("win");
+    document.querySelector("#c2").classList.add("win");
+    document.querySelector("#c4").classList.add("win");
+    document.querySelector("#c6").classList.add("win");
     return threeboard[2];
   }
   //returns nothing in case no winner
@@ -77,6 +83,8 @@ const check_match_3 = () => {
 
 const check_for_winner = () => {
   let res = check_match_3()
+  if(vsHuman==0)
+ {
   if (res == player) {
     winner.innerText = "You win!";
     winner.classList.add("playerWin");
@@ -89,6 +97,21 @@ const check_for_winner = () => {
     winner.innerText = "Draw!";
     winner.classList.add("draw");
   }
+}
+else {
+    if (res == player) {
+      winner.innerText = "Player 1 win!";
+      winner.classList.add("playerWin");
+      board_full = true
+    } else if (res == computer) {
+      winner.innerText = "Player 2 win!";
+      winner.classList.add("computerWin");
+      board_full = true
+    } else if (board_full) {
+      winner.innerText = "Draw!";
+      winner.classList.add("draw");
+    }
+}
 };
 
 
@@ -97,7 +120,7 @@ const render_board_3 = () => {
   threeboard.forEach((e, i) => {
     board_container.innerHTML += `<div id="c${i}" class="block" onclick="addPlayerMove3(${i})">${threeboard[i]}</div>`
     if (e == player || e == computer) {
-      document.querySelector(`#b${i}`).classList.add("occupied");
+      document.querySelector(`#c${i}`).classList.add("occupied");
     }
   });
 };
@@ -106,27 +129,68 @@ const render_board_4 = () => {
   fourboard.forEach((e, i) => {
     board_container.innerHTML += `<div id="b${i}" class="block" onclick="addPlayerMove4(${i})">${fourboard[i]}</div>`
     if (e == player || e == computer) {
-      document.querySelector(`#c${i}`).classList.add("occupied");
+      document.querySelector(`#b${i}`).classList.add("occupied");
     }
   });
 };
-
+//first move by computer
+const firstComputerMove_3 = () => {
+    player = "O";
+    computer = "X";
+    reset_board_3();
+    selected = Math.floor(Math.random() * 9);
+    threeboard[selected] = computer;
+    game_loop_3();
+};
+//first move by Human
+const  firstHumanMove_3 = () => {
+    player = "X";
+    computer = "O";
+    reset_board_3();
+};
+//vsHumans setting
+const twoHumanPlayer = () => {
+    vsHuman = 1;
+    reset_board_3();
+};
+const oneHumanPlayer = () => {
+    vsHuman = 0;
+    reset_board_3();
+};
+//
 const game_loop_3 = () => {
   render_board_3();
   check_board_complete();
   check_for_winner();
-}
+};
 const game_loop_4 = () => {
   render_board_4();
   check_board_complete();
   check_for_winner();
-}
+};
 //adding player moves
 const addPlayerMove3 = e => {
   if (!board_full && threeboard[e] == "") {
-    threeboard[e] = player;
-    game_loop_3();
-    addComputerMove3();
+    if(vsHuman==0)
+    {
+        threeboard[e] = player;
+        game_loop_3();
+        addComputerMove3();
+    }
+    else
+    {
+        if(move==1)
+        {
+            threeboard[e] = player1;
+            move=2;
+            game_loop_3();
+        }
+        else {
+            threeboard[e] = player2;
+            move=1;
+            game_loop_3();
+        }
+    }
   }
 };
 const addPlayerMove4 = e => {
@@ -143,6 +207,7 @@ const oneDtotwoD_3 = (threeboard) => {
   {
       twoD[i] = [threeboard[(i*3)],threeboard[(i*3)+1],threeboard[(i*3)+2]];
   }
+  console.log(twoD);
   return twoD;
 };
 
@@ -151,7 +216,7 @@ const isMoveLeft = (board) => {
         {
             for(var j=0;j<3;j++)
             {
-                if(board[i][j]=='_'){
+                if(board[i][j]==''){
                     return "true";
                 }
             }
@@ -165,11 +230,11 @@ const checkGameOver = (board,depth) => {
         {
                 if(board[i][0]==board[i][1] && board[i][0]==board[i][2])
                 {
-                    if(board[i][0]==this.player)
+                    if(board[i][0]==computer)
                     {
                         return 1000-depth;
                     }
-                    if(board[i][0]==this.opponent)
+                    if(board[i][0]==player)
                     {
                         return -1000+depth;
                     }
@@ -179,11 +244,11 @@ const checkGameOver = (board,depth) => {
         {
                 if(board[0][i]==board[1][i] && board[0][i]==board[2][i])
                 {
-                    if(board[0][i]==this.player)
+                    if(board[0][i]==computer)
                     {
                         return 1000-depth;
                     }
-                    if(board[0][i]==this.opponent)
+                    if(board[0][i]==player)
                     {
                         return -1000+depth;
                     }
@@ -191,22 +256,22 @@ const checkGameOver = (board,depth) => {
         }
         if(board[0][0]==board[1][1] && board[0][0]==board[2][2])
         {
-            if(board[0][0]==this.player)
+            if(board[0][0]==computer)
             {
                 return 1000-depth;
             }
-            if(board[0][0]==this.opponent)
+            if(board[0][0]==player)
             {
                 return -1000+depth;
             }
         }
         if(board[2][0]==board[1][1] && board[2][0]==board[0][2])
         {
-            if(board[0][2]==this.player)
+            if(board[0][2]==computer)
             {
                 return 1000-depth;
             }
-            if(board[0][2]==this.opponent)
+            if(board[0][2]==player)
             {
                 return -1000+depth;
             }
@@ -222,11 +287,11 @@ const evaluate = (board,depth) => {
             p[i]=0;
             for(var j=0;j<3;j++)
             {
-                if(board[i][j]==this.player)
+                if(board[i][j]==player)
                 {
                     p[i]+=1;
                 }
-                if(board[i][j]==this.opponent)
+                if(board[i][j]==computer)
                 {
                     o[i]+=1;
                 }
@@ -237,11 +302,11 @@ const evaluate = (board,depth) => {
             p[j+3]=0;
             for(var i=0;i<3;i++)
             {
-                if(board[i][j]==this.player)
+                if(board[i][j]==player)
                 {
                     p[j+3]+=1;
                 }
-                if(board[i][j]==this.opponent)
+                if(board[i][j]==computer)
                 {
                     o[j+3]+=1;
                 }
@@ -251,11 +316,11 @@ const evaluate = (board,depth) => {
         o[6]=0;
         for(var i=0;i<3;i++)
         {
-            if(board[i][i]==this.player)
+            if(board[i][i]==player)
             {
                 p[6]+=1;
             }
-            if(board[i][i]==this.opponent)
+            if(board[i][i]==computer)
             {
                 o[6]+=1;
             }
@@ -264,11 +329,11 @@ const evaluate = (board,depth) => {
         o[7]=0;
         for(var i=0;i<3;i++)
         {
-            if(board[i][2-i]==this.player)
+            if(board[i][2-i]==player)
             {
                 p[7]+=1;
             }
-            if(board[i][2-i]==this.opponent)
+            if(board[i][2-i]==computer)
             {
                 o[7]+=1;
             }
@@ -300,14 +365,15 @@ const evaluate = (board,depth) => {
 
 const minimax = (board,isMax,depth,maxDepth,alpha,beta) => {
         //board[3]+=1;
-        var score = this.checkGameOver(board,depth);
+        tot+=1;
+        var score = checkGameOver(board,depth);
         //console.log("score at ");
         //console.log(score);
         if(score!=0)
         {
             return [score,1];
         }
-        if(this.isMoveLeft(board)=="false")
+        if(isMoveLeft(board)=="false")
         {
             //console.log("this is working");
             return [0,0];
@@ -325,15 +391,15 @@ const minimax = (board,isMax,depth,maxDepth,alpha,beta) => {
             {
                 for(var j=0;j<3;j++)
                 {
-                    if (board[i][j]=='_')
+                    if (board[i][j]=='')
                     {
-                        board[i][j]=this.player;
-                        val = this.minimax(board,"false",depth+1,maxDepth,alpha,beta);
-                        board[i][j]='_';
+                        board[i][j]=computer;
+                        val = minimax(board,"false",depth+1,maxDepth,alpha,beta);
+                        board[i][j]='';
                         best = Math.max(best,val[0]);
                         alpha = Math.max(alpha,best);
                         allOpt.push(val);
-                        if(beta<alpha)
+                        if(beta<=alpha)
                         {
                             br=1;
                             break;
@@ -345,7 +411,7 @@ const minimax = (board,isMax,depth,maxDepth,alpha,beta) => {
                     break;
                 }
             }
-            for(let i=0;i<allOpt.length;i++)
+            for(var i=0;i<allOpt.length;i++)
             {
                 if(allOpt[i][0]==best)
                 {
@@ -365,16 +431,16 @@ const minimax = (board,isMax,depth,maxDepth,alpha,beta) => {
             {
                 for(var j=0;j<3;j++)
                 {
-                    if (board[i][j]=='_')
+                    if (board[i][j]=='')
                     {
-                        board[i][j]=this.opponent;
-                        val = this.minimax(board,"true",depth+1,maxDepth,alpha,beta);
-                        board[i][j]='_';
+                        board[i][j]=player;
+                        val = minimax(board,"true",depth+1,maxDepth,alpha,beta);
+                        board[i][j]='';
                         best = Math.min(best,val[0]);
                         beta =  Math.min(best,beta);
                         //console.log(val);
                         allOpt.push(val);
-                        if(beta<alpha)
+                        if(beta<=alpha)
                         {
                             br=1;
                             break;
@@ -398,19 +464,20 @@ const minimax = (board,isMax,depth,maxDepth,alpha,beta) => {
     }
     else {
 
-        ans = this.evaluate(board,depth);
+        ans = evaluate(board,depth);
         return [ans,0];
     }
 };
 
-const getBestMove = () =>{
+const getBestMove = (maxDepth) =>{
         //board[3]+=1;
+        tot+=1;
         var board = oneDtotwoD_3(threeboard);
         var alpha = -10000;
         var beta = 10000;
-        var maxDepth=3;
-        allOpt =[];
-        bestOpt = [];
+        //var maxDepth=11;
+        var allOpt =[];
+        var bestOpt = [];
         var bestAns= -1000;
         var ans = [-1,-1]
         var moveAns;
@@ -418,11 +485,11 @@ const getBestMove = () =>{
         {
             for(var j=0;j<3;j++)
             {
-                if (board[i][j]=='_')
+                if (board[i][j]=='')
                 {
-                    board[i][j]=player;
+                    board[i][j]=computer;
                     moveAns = minimax(board,"false",1,maxDepth,alpha,beta);
-                    board[i][j]='_';
+                    board[i][j]='';
                     //console.log(moveAns);
                     if (moveAns[0]>bestAns)
                     {
@@ -444,7 +511,7 @@ const getBestMove = () =>{
             }
         }
         var loc=0;
-        for(var i=0;i<bestOpt.length;i++)
+        for(var i=1;i<bestOpt.length;i++)
         {
             if(bestOpt[i][0][1]>bestOpt[loc][0][1])
             {
@@ -455,17 +522,27 @@ const getBestMove = () =>{
         //console.log(bestOpt);
         var answer = [bestOpt[loc][1],bestOpt[loc][2]];
         //console.log(answer);
-        var final_ans = answer[0]*3 + answer[1];
+        console.log(tot);
+        var final_ans = (answer[0])*3 + answer[1];
         return final_ans;
     };
-// Eva
-// call getBestMove from addComputerMove3 func
-// return value is the index in 1D array which is the solution
 const addComputerMove3 = () => {
   if (!board_full) {
-    do {
+    /*do {
       selected = Math.floor(Math.random() * 9);
-    } while (threeboard[selected] != "");
+  } while (threeboard[selected] != "");*/
+  if(level == 1 )
+  {
+      selected = level_1();
+  }
+  if(level == 2)
+  {
+       selected = level_2();
+  }
+  if(level == 3)
+  {
+       selected = level_3();
+  }
     threeboard[selected] = computer;
     game_loop_3();
   }
@@ -479,10 +556,31 @@ const addComputerMove4 = () => {
     game_loop_4();
   }
 };
+//Level based
+const level_1 = () => {
+    do {
+      selected = Math.floor(Math.random() * 9);
+  } while (threeboard[selected] != "");
+  return selected;
+};
+const level_2 = () => {
+    selected = getBestMove(1);
+    return selected;
+};
+const level_3 = () => {
+        selected = getBestMove(10);
+        return selected;
+};
+//decide level
+const chooseLevel = (num) =>{
+    level = num;
+    reset_board_3();
+};
 //reset board
 const reset_board_3 = () => {
   threeboard = ["", "", "", "", "", "", "", "", ""];
   board_full = false;
+  move = 1;
   winner.classList.remove("playerWin");
   winner.classList.remove("computerWin");
   winner.classList.remove("draw");
